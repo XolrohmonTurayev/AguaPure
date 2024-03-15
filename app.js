e5aec5c140634140a90f37b30ae2aaf2538947f3
Xolrahmon <jaggedstone071.@gmail.com>
2024-03-15 17:05:13 +0500

    added some functionals

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 // HELLO JavaScript...
 
-//burgerMenu settings!
+//Variables!
 let burgerIcon = document.getElementById("burger-menu-icon");
 let burgerMenu = document.getElementById("burger-menu");
+let closeButton = document.getElementById("close-button");
 
+closeButton, burgerMenu.classList.toggle("visible");
+
+
+//Burger menu settings!
 burgerIcon.addEventListener("click", () => {
-    burgerMenu.classList.toggle("visible");
     if (burgerMenu.classList.contains("visible")) {
-        burgerMenu.style.transform = "translateX(0)"
+        burgerMenu.style.transform = "translateX(0)";
     } else {
         burgerMenu.style.transform = "translateX(-100%)"
     }
 });
 
+//Close button settings!
+closeButton.addEventListener("click", () => {
+    if (burgerMenu.classList.contains("visible")) {
+        burgerMenu.style.transform = "translateX(-100%)";
+    } else {
+        burgerMenu.style.transform = "translateX(0)"
+    }
+});
+
+//Searching modal settings!
+let searchButton = document.getElementById("search-button")
+let searchModal = document.getElementById("search-modal");
+
+searchButton.classList.toggle("search");
+
+searchButton.addEventListener("click", () => {
+    if (searchModal.classList.contains("search")) {
+        searchModal.style.transform = "translateY(250%)";
+    } else {
+        searchModal.style.transform = "translateY(0)";
+    }
+});
+
+//Transition!
+searchModal.style.transition = "0.3s"
 burgerMenu.style.transition = "0.4s";
