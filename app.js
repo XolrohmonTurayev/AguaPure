e5368cdf69bbe908d40f8124cf689cb3a069d49a
Xolrahmon <jaggedstone071.@gmail.com>
2024-03-25 15:29:23 +0500

    yena JavaScripta qarab qoldi ishing

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 // HELLO JavaScript...
 
-//Variables!
+let closeButton = document.getElementById("close-button");
 let burgerIcon = document.getElementById("burger-menu-icon");
 let burgerMenu = document.getElementById("burger-menu");
-let closeButton = document.getElementById("close-button");
 let searchButton = document.getElementById("search-button")
 let searchModal = document.getElementById("search-modal");
-let closeModal = document.getElementById("close-modal")
+let closeModal = document.getElementById("close-modal");
+let viewMassages = document.getElementById("view-massages");
+let massagesPopUp = document.getElementById("massage-pop-up");
+let quantity = document.getElementById("quantity-input").value;
+let waterPrice = 12.5;
+
+
+
+function calcPrice() {
+    return quantity * waterPrice;
+}
+
+
+console.log(calcPrice());
 
 closeButton, searchButton, burgerMenu.classList.toggle("visible");
-
-
 //Burger menu settings!
+
 burgerIcon.addEventListener("click", () => {
     if (burgerMenu.classList.contains("visible")) {
         burgerMenu.style.transform = "translateX(0)";
@@ -21,6 +32,7 @@ burgerIcon.addEventListener("click", () => {
 });
 
 //Close button settings!
+
 closeButton.addEventListener("click", () => {
     if (burgerMenu.classList.contains("visible")) {
         burgerMenu.style.transform = "translateX(-100%)";
@@ -30,14 +42,42 @@ closeButton.addEventListener("click", () => {
 });
 
 //Searching modal settings!
+
 searchButton.addEventListener("click", () => {
     searchModal.style.transform = "translateY(0)";
 });
 //close Modal
+
 closeModal.addEventListener("click", () => {
     searchModal.style.transform = "translateY(-200%)";
-})
+});
 
-//Transition!
+//View massages button settings!
+
+viewMassages.addEventListener('click', () => {
+    massagesPopUp.classList.toggle("visible");
+    if (massagesPopUp.classList.contains("visible")) {
+        viewMassages.textContent = 'hide messages'
+    } else {
+        viewMassages.textContent = "view massage"
+    }
+
+});
+
+
+//Transitions!
 searchModal.style.transition = "0.3s"
 burgerMenu.style.transition = "0.4s";
+massagesPopUp.style.transition = "0.3s"
+
+
+// swiper settings!
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
